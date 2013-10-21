8d26faada3a615a63a25846750b9d65bc340b0db
JustinGA <justinkemmerer@gmail.com>
2013-10-21 14:15:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Glassevents::Application.config.secret_key_base = '1470b0c7c16abd0a356466fda0c8706a67a4ee29421d83c313079c57c6a803f9462a5bc4d6e00b5ca90a32c8aae28ba070451624707420651e127e321c6b4e89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Glassevents::Application.config.session_store :cookie_store, key: '_glassevents_session'
