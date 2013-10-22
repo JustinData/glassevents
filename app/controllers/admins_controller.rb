49b6877364f90a2ac3470380e7598a0454fb3066
JustinGA <justinkemmerer@gmail.com>
2013-10-22 19:50:59 -0400

    admin show page rejects non-admin ID and reroutes to the admins list

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -6,5 +6,9 @@ class AdminsController < ApplicationController
 
 	def show
 		@users = User.find(session[:user_id])
+		user = User.find(params[:id])
+		if user.admin == false 
+			redirect_to admins_path 
+		end
 	end
 end
