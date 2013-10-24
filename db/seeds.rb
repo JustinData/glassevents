0784aa66f0722344c6e89cdbb70940364fc99c1e
JustinGA <justinkemmerer@gmail.com>
2013-10-24 14:54:27 -0400

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(first_name: 'Jason', last_name: 'Kemmerer', email: 'gpsxsirus@gmail.com', owner: true, admin: true, password: 'decaff', password_confirmation: 'decaff')
