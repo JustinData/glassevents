dff1a3c8034ba6e8907186c4428f965c2c33da8c
JustinGA <justinkemmerer@gmail.com>
2013-10-25 09:51:22 -0400

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,12 @@ Quote.destroy_all
 Service.destroy_all
 User.destroy_all
 User.create(first_name: 'Justin', last_name: 'Kemmerer', email: 'icacnt@gmail.com', owner: true, admin: true, password: 'decaff', password_confirmation: 'decaff')
+User.create(first_name: 'Joe', last_name: 'Smoe', email: 'joe@shmoe.com', admin: true, password: 'decaff', password_confirmation: 'decaff')
+User.create(first_name: 'John', last_name: 'Doe', email: 'john@doe.com', admin: true, password: 'decaff', password_confirmation: 'decaff')
+User.create(first_name: 'Jane', last_name: 'Doe', email: 'jane@doe.com', admin: true, password: 'decaff', password_confirmation: 'decaff')
+User.create(first_name: 'Bob', last_name: 'Barker', email: 'bob@barker.com', admin: true, password: 'decaff', password_confirmation: 'decaff')
+
+Service.create(service_name: 'Venue')
+Service.create(service_name: 'Catering')
+Service.create(service_name: 'DJ')
+Service.create(service_name: 'Phtographer')
